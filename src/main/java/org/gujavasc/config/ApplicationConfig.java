aafe3b8345545fc0c80b21f0dbf5728e97482430
ivanjunckes <ivanjunckes@gmail.com>
2014-05-01 14:15:53 -0300

    Adjusting project

---
+++ src/main/java/org/gujavasc/config/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package org.gujavasc.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("resources")
+public class ApplicationConfig extends Application {
+ 
+}
