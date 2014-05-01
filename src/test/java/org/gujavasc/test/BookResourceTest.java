163e7011e1b53ede29d3252c5d2d75c8dee209aa
Ricardo Longa <ricardo.longa@gmail.com>
2014-05-01 16:38:57 -0300

    criacao do primeiro teste

--- src/test/java/org/gujavasc/test/BookResourceTest.java
+++ src/test/java/org/gujavasc/test/BookResourceTest.java
@@ -1,9 +1,52 @@
 package org.gujavasc.test;
 
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Collection;
+import java.util.List;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.commons.io.IOUtils;
+import org.gujavasc.entity.Book;
+import org.junit.Test;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 public class BookResourceTest {
 
-    public void test() {
+    @Test
+    public void javaee6() throws Exception {
+        URL url = new URL("http://localhost:8080/rest-example/resources/books");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        String result = IOUtils.toString(con.getInputStream(), "UTF-8");
+        con.disconnect();
 
+        List<Book> books = new Gson().fromJson(result, new TypeToken<Collection<Book>>() {}.getType());
+
+        assertThat(books, not(empty()));
     }
+
+    //@formatter:off
+    @Test
+    public void javaee7() throws Exception {
+        Client client = ClientBuilder.newClient();
+      
+        List<Book> books = client.target("http://localhost:8080/rest-example/resources")
+                                 .path("books")
+                                 .request(MediaType.APPLICATION_JSON)
+                                 .get(new GenericType<List<Book>>(){});
+        
+        assertThat(books, not(empty()));
+    }
+    //@formatter:on
 
 }
